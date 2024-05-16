c77974263aac65805a2efb87ad39ac77a811c6c8
spankaj13579 <spankaj13579@gmail.com>
2024-05-16 07:57:51 +0530

    improved bottom nav

--- src/component/Bottomnav.jsx
+++ src/component/Bottomnav.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BsFillHouseDoorFill } from "react-icons/bs";
+import { FaPlus } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 
 const Bottomnav = () => {
   return (
-    <div className="flex justify-between gap-x-3 bg-white py-6 px-6">
+    <div className="flex relative justify-between gap-x-3 bg-white py-6 px-6">
       <div className="flex flex-col items-center gap-2 text-red-600">
         <BsFillHouseDoorFill />
         <p className="text-sm">Dashboard</p>
       </div>
+      <div className="bg-red-600 absolute left-1/2 top-1/4 transform -translate-x-1/2 -translate-y-1/2 text-white p-6 rounded-full border-[12px] border-slate-100">
+        <FaPlus />
+      </div>
       <div className="flex flex-col items-center gap-2 text-red-600">
-      <IoMdSettings />
+        <IoMdSettings />
         <p className="text-sm">settings</p>
       </div>
     </div>
