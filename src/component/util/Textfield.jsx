c224b316d22b3f8f6cd0e697c7975947c6156913
spankaj13579 <spankaj13579@gmail.com>
2024-05-21 22:55:54 +0530

    date function created

--- src/component/util/Textfield.jsx
+++ src/component/util/Textfield.jsx
@@ -3,13 +3,17 @@ import React, { useEffect, useState } from 'react';
 const Textfield = ({ type = 'text', label, placeholder, min, max }) => {
   const [todaysDate, settodaysDate] = useState('');
 
-  useEffect(() => {
+  let currentDate= ()=>{
     const date = new Date();
     const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based, so add 1
+    const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
     const currentDate = `${year}-${month}-${day}`;
     settodaysDate(currentDate);
+    }
+    
+  useEffect(() => {
+    currentDate();
   }, []);
 
   return (
