be5d2d9c8ea5a49a5d70fac3a18d0c70e3a499dc
spankaj13579 <spankaj13579@gmail.com>
2024-05-21 22:03:20 +0530

    improved scss

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = () => {
     <div className='bg-slate-600 bg-opacity-60 h-full w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-between'>
       <div className='modal-body relative rounded-2xl bg-slate-100 p-5 flex flex-col h-100 justify-between w-11/12 m-auto items-center gap-y-10 '>
         <div className='modal-head'>
-        <p className='absolute right-4 top-3'>
+        <p className='absolute right-4 top-3 cursor-pointer'>
             <RxCross2 />
           </p>
           <h1 className='text-base font-semibold'>Add New Category</h1>
